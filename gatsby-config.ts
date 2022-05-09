08d4c37abe32aedd1f27888b26f591a95bfae4f8
Gabriel <smolana.gabriel@gmail.com>
2022-05-09 22:00:52 +0200

    Added types

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,33 +1,34 @@
-require('dotenv').config()
 import type { GatsbyConfig } from 'gatsby'
 
+require('dotenv').config()
+
 const strapiConfig = {
-  apiURL: process.env.STRAPI_API_URL,
   accessToken: process.env.STRAPI_TOKEN,
-  collectionTypes: ['post', 'speaker', 'partner'],
+  apiURL: process.env.STRAPI_API_URL,
+  collectionTypes: ['post', 'partner', 'speaker'],
   singleTypes: [],
 }
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `bbdays-website`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
   plugins: [
     'gatsby-plugin-sass',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sitemap',
     {
-      resolve: 'gatsby-plugin-manifest',
       options: {
         icon: 'src/images/icon.png',
       },
+      resolve: 'gatsby-plugin-manifest',
     },
     {
-      resolve: 'gatsby-source-strapi',
       options: strapiConfig,
+      resolve: 'gatsby-source-strapi',
     },
   ],
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`,
+    title: `BB Days 4 IT`,
+  },
 }
 
 export default config
