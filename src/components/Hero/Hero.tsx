aa9a7905699df7408e353cf90d048f1307eeb117
Dawid Holisz <dawid.holisz@gmail.com>
2022-05-17 15:58:10 +0200

    add stylelint

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,20 +3,18 @@ import React, { FC } from 'react'
 
 import { BaseHeroType } from 'types'
 
-const Hero: FC<BaseHeroType> = ({ title, subtitle, backgroundImage, buttonText, buttonUrl, backgroundColor }) => {
-  return (
-    <div
-      className={classnames('hero', { '-full-height': subtitle !== null })}
-      style={{ backgroundColor: backgroundColor, backgroundImage: `url(${backgroundImage?.url}` }}>
-      <h3 className="hero__subtitle">{subtitle}</h3>
-      <h1 className={classnames('hero__title', { '-small': subtitle == null })}>{title}</h1>
-      {buttonText !== null && buttonUrl !== null && (
-        <a className="hero__button" href={buttonUrl} rel="noopener noreferrer" target="_blank">
-          {buttonText}
-        </a>
-      )}
-    </div>
-  )
-}
+const Hero: FC<BaseHeroType> = ({ title, subtitle, backgroundImage, buttonText, buttonUrl, backgroundColor }) => (
+  <div
+    className={classnames('hero', { '-full-height': subtitle !== null })}
+    style={{ backgroundColor: backgroundColor, backgroundImage: `url(${backgroundImage?.url}` }}>
+    <h3 className="hero__subtitle">{subtitle}</h3>
+    <h1 className={classnames('hero__title', { '-small': subtitle == null })}>{title}</h1>
+    {buttonText !== null && buttonUrl !== null && (
+      <a className="hero__button" href={buttonUrl} rel="noopener noreferrer" target="_blank">
+        {buttonText}
+      </a>
+    )}
+  </div>
+)
 
 export default Hero
