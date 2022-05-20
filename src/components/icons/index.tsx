ab390c63088d0c39fb1ed0fe793a881c0702f63d
Gabriel <smolana.gabriel@gmail.com>
2022-05-20 19:04:08 +0200

    update exports

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,19 +1,7 @@
-import BBDaysLogoLight from './bbdays-logo-light.svg'
-import BBDaysLogoDark from './bbdays-logo-dark.svg'
-
-import HamburgerIcon from './hamburger-icon.svg'
-import CloseHamburgerIcon from './hamburger-close-icon.svg'
-
-import FacebookIcon from './facebook-icon.svg'
-import TwitterIcon from './twitter-icon.svg'
-import InstagramIcon from './instagram-icon.svg'
-
-export {
-    BBDaysLogoLight,
-    BBDaysLogoDark,
-    HamburgerIcon,
-    CloseHamburgerIcon,
-    FacebookIcon,
-    TwitterIcon,
-    InstagramIcon
-}
+export { default as BBDaysLogoLight } from './bbdays-logo-light.svg'
+export { default as BBDaysLogoDark } from './bbdays-logo-dark.svg'
+export { default as HamburgerIcon } from './hamburger-icon.svg'
+export { default as CloseHamburgerIcon } from './hamburger-close-icon.svg'
+export { default as TwitterIcon } from './twitter-icon.svg'
+export { default as FacebookIcon } from './facebook-icon.svg'
+export { default as InstagramIcon } from './instagram-icon.svg'
