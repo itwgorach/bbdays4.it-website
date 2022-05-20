3f2e82fcd1fece2f77b2abd3dce789947f21f5de
Gabriel <smolana.gabriel@gmail.com>
2022-05-20 08:59:38 +0200

    add getPartnerWhiteLogo util

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export { default as BBDaysLogoLight } from './bbdays-logo-light.svg'
 export { default as BBDaysLogoDark } from './bbdays-logo-dark.svg'
 export { default as HamburgerIcon } from './hamburger-icon.svg'
@@ -6,24 +5,3 @@ export { default as CloseHamburgerIcon } from './hamburger-close-icon.svg'
 export { default as TwitterIcon } from './twitter-icon.svg'
 export { default as FacebookIcon } from './facebook-icon.svg'
 export { default as InstagramIcon } from './instagram-icon.svg'
-=======
-import BBDaysLogoLight from './bbdays-logo-light.svg'
-import BBDaysLogoDark from './bbdays-logo-dark.svg'
-
-import HamburgerIcon from './hamburger-icon.svg'
-import CloseHamburgerIcon from './hamburger-close-icon.svg'
-
-import FacebookIcon from './facebook-icon.svg'
-import TwitterIcon from './twitter-icon.svg'
-import InstagramIcon from './instagram-icon.svg'
-
-export {
-    BBDaysLogoLight,
-    BBDaysLogoDark,
-    HamburgerIcon,
-    CloseHamburgerIcon,
-    FacebookIcon,
-    TwitterIcon,
-    InstagramIcon
-}
->>>>>>> dd2a9a8 (add icon exports)
--- src/utils/getPartnerWhiteLogo.ts
+++ src/utils/getPartnerWhiteLogo.ts
@@ -4,6 +4,9 @@ export function getPartnerWhiteLogo( partner: PartnerType ) {
     if(!partner.whiteLogo?.url) {
         return partner.Logo?.url
     }
+<<<<<<< HEAD
 
     return partner.whiteLogo.url
+=======
+>>>>>>> 1059529 (add getPartnerWhiteLogo util)
 }
