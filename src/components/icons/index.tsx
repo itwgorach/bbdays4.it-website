dd2a9a8e774bf8e257691c2f276b2504c578d4b7
Gabriel <smolana.gabriel@gmail.com>
2022-05-20 08:59:03 +0200

    add icon exports

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,19 @@
+import BBDaysLogoLight from './bbdays-logo-light.svg'
+import BBDaysLogoDark from './bbdays-logo-dark.svg'
+
+import HamburgerIcon from './hamburger-icon.svg'
+import CloseHamburgerIcon from './hamburger-close-icon.svg'
+
+import FacebookIcon from './facebook-icon.svg'
+import TwitterIcon from './twitter-icon.svg'
+import InstagramIcon from './instagram-icon.svg'
+
+export {
+    BBDaysLogoLight,
+    BBDaysLogoDark,
+    HamburgerIcon,
+    CloseHamburgerIcon,
+    FacebookIcon,
+    TwitterIcon,
+    InstagramIcon
+}
