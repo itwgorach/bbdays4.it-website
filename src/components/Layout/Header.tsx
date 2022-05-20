42768ef258b0d9be2d56f5325ebbac5fc97cfd84
Gabriel <smolana.gabriel@gmail.com>
2022-05-20 19:04:08 +0200

    update exports

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -46,47 +46,11 @@ const linksData = [
 const Header: FC = () => {
   const isMobile = useMediaQuery({ maxWidth: 992 })
 
-<<<<<<< HEAD
   if(isMobile) {
     return <MobileHeader links={linksData} />
   }
   
   return <DesktopHeader links={linksData} />
-=======
-  return (
-    <header className={classnames('header', { '-hamburger-open': isMenuOpen })}>
-      <div className="header__inner">
-        <BBDaysLogoDark className="header__logo -mobile" />
-        <BBDaysLogoLight className="header__logo -desktop" />
-        <div className="header__menu">
-          <ul className="header__nav">
-            {linksData.map((link) =>  (
-                <li key={link.name}>
-                  <Link to={link.path} className="header__nav-link" onClick={() => setIsMenuOpen(false)}>
-                    {link.name}
-                  </Link>
-                </li>
-              )
-            )}
-          </ul>
-          <button className="header__contact-button">Skontaktuj się z nami</button>
-          <div className="header__socials">
-            <TwitterIcon />
-            <FacebookIcon />
-            <InstagramIcon />
-          </div>
-        </div>
-        <div className="header__hamburger-menu">
-          {isMenuOpen ? (
-            <CloseHamburgerIcon className="header__hamburger-icon" onClick={() => setIsMenuOpen(!isMenuOpen)} />
-          ) : (
-            <HamburgerIcon className="header__hamburger-icon" onClick={() => setIsMenuOpen(!isMenuOpen)} />
-          )}
-        </div>
-      </div>
-    </header>
-  )
->>>>>>> 5d9fb3d (update styles and markup)
 }
 
 export default Header
