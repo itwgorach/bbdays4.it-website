c051865a446af346d7635d8e6cf7f1a7ebabf74a
Gabriel <smolana.gabriel@gmail.com>
2022-05-23 13:11:35 +0200

    delete wrong util

--- src/utils/getPartnersWhiteLogo.ts
+++
@@ -1,9 +0,0 @@
-import { PartnerType } from "types"
-
-export function getPartnerWhiteLogo( partner: PartnerType ) {
-    if(!partner.whiteLogo?.url) {
-        return partner.Logo?.url
-    }
-
-    return partner.whiteLogo.url
-}
