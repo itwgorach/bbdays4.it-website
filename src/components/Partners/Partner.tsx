62fb31a395df0270f9019f40429b855d3b0fe920
Gabriel <smolana.gabriel@gmail.com>
2022-05-23 13:33:39 +0200

    update partner component structure

--- src/components/Partners/Partner.tsx
+++ src/components/Partners/Partner.tsx
@@ -3,15 +3,13 @@ import React, { FC } from 'react'
 
 import { PartnerType } from 'types'
 
-const Partner: FC<PartnerType> = ({ Name, WebsiteURL, Logo, whiteLogo }: PartnerType) => {
-  return (
-    <>
-      <a className="partners__link" href={WebsiteURL} rel='noopener noreferrer' target='_blank'>
-        <img alt={Name} className="partners__link-image -mobile" src={Logo?.url}/>
-        <img alt={Name} className="partners__link-image -desktop" src={whiteLogo}/>
-      </a>
-    </>
-  )
-}
+const Partner: FC<PartnerType> = ({ Name, WebsiteURL, Logo, whiteLogo }: PartnerType) => (
+  <>
+    <a className="partners__link" href={WebsiteURL} rel="noopener noreferrer" target="_blank">
+      <img alt={Name} className="partners__link-image -mobile" src={Logo?.url} />
+      <img alt={Name} className="partners__link-image -desktop" src={whiteLogo} />
+    </a>
+  </>
+)
 
 export default Partner
