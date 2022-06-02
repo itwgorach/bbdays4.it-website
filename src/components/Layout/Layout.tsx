23892b8e97d193578408d482c1c84d270b924d5b
Robert Harężlak <harezlak.robert7@gmail.com>
2022-06-02 11:13:47 +0200

    chore: add useEffect

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,26 @@
-import React, { FC } from 'react'
-import { useMediaQuery } from 'react-responsive'
+import React, { FC, useEffect, useState } from 'react'
 
 import Header from './Header'
 import Seo from './Seo'
 
 const Layout: FC<JSX.IntrinsicAttributes> = ({ children, ...props }) => {
-  const isDesktop = useMediaQuery({ minWidth: 1224 })
+  const [matches, setMatches] = useState(false)
+
+  useEffect(() => {
+    const media = window.matchMedia('(min-width: 1224px)')
+    if (media.matches !== matches) {
+      setMatches(media.matches)
+    }
+    const listener = () => {
+      setMatches(media.matches)
+    }
+    media.addListener(listener)
+    return () => media.removeListener(listener)
+  }, [matches])
   return (
     <>
       <Seo />
-      <Header isDesktop={isDesktop} />
+      <Header isDesktop={matches} />
       <main className="layout">{React.cloneElement(children, props)}</main>
     </>
   )
