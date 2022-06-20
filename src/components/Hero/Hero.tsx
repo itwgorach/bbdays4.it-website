5c6477b5fb1ffc4bfaf4739bac3e247ab59ea6e2
jaworskig00 <2000grzegorz@gmail.com>
2022-06-20 14:37:55 +0200

    fix: small landpage fixes

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,15 +3,16 @@ import React, { FC } from 'react'
 
 import { BaseHeroType } from 'types'
 
-const Hero: FC<BaseHeroType> = ({ title, subtitle, backgroundImage, buttonText, buttonUrl, backgroundColor }) => (
+const Hero: FC<BaseHeroType> = ({ title, subtitle, subtitlePos, backgroundImage, buttonText, buttonUrl, backgroundColor }) => (
   <div
     className={classnames('hero', {
       '-full-height': subtitle !== null,
       '-simple': subtitle === null && buttonText === null && buttonUrl === null,
     })}
     style={{ backgroundColor: backgroundColor, backgroundImage: `url(${backgroundImage?.url}` }}>
-    <h3 className="hero__subtitle">{subtitle}</h3>
+    {subtitlePos === 'top' && <h3 className="hero__subtitle">{subtitle}</h3>}
     <h1 className={classnames('hero__title', { '-small': subtitle == null })}>{title}</h1>
+    {subtitlePos === 'bottom' && <h3 className="hero__subtitle -bottom">{subtitle}</h3>}
     {buttonText !== null && buttonUrl !== null && (
       <a className="hero__button" href={buttonUrl} rel="noopener noreferrer" target="_blank">
         {buttonText}
