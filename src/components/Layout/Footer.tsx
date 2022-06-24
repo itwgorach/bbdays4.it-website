de8520f8a2f5b1608dcc7483d209fff333d2fe6f
jaworskig00 <2000grzegorz@gmail.com>
2022-06-24 12:56:37 +0200

    fix: mobile first styles and social links export

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,19 +1,16 @@
+import { links as socialLinks } from 'constants/constants'
 import React, { FC } from 'react'
-
-import { FacebookIcon, InstagramIcon, TwitterIcon } from 'components/icons'
 
 const Footer: FC = () => {
   return (
     <footer className="footer">
-      <a href="https://twitter.com/bbdays4" rel="noopener noreferrer" target="_blank">
-        <TwitterIcon className="footer__social-icon" />
-      </a>
-      <a href="https://www.facebook.com/bbdays4.it/" rel="noopener noreferrer" target="_blank">
-        <FacebookIcon className="footer__social-icon" />
-      </a>
-      <a href="https://www.instagram.com/bbdays4.it/" rel="noopener noreferrer" target="_blank">
-        <InstagramIcon className="footer__social-icon" />
-      </a>
+      {socialLinks.map(({ href, Icon }) => {
+        return (
+          <a key={href} href={href} rel="noopener noreferrer" target="_blank">
+            <Icon className="footer__social-icon" />
+          </a>
+        )
+      })}
     </footer>
   )
 }
---
+++ src/constants/constants.tsx
@@ -0,0 +1,16 @@
+import { InstagramIcon, FacebookIcon, TwitterIcon } from 'components/icons'
+
+export const links = [
+  {
+    href: 'https://twitter.com/bbdays4',
+    Icon: TwitterIcon,
+  },
+  {
+    href: 'https://www.facebook.com/bbdays4.it/',
+    Icon: FacebookIcon,
+  },
+  {
+    href: 'https://www.instagram.com/bbdays4.it/',
+    Icon: InstagramIcon,
+  },
+]
