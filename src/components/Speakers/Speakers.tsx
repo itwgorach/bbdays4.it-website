23dbff8c32b9bd582ebca992bac46e8ac748bc13
Patryk <47920348+Matasin@users.noreply.github.com>
2022-07-04 20:05:08 +0200

    feat: add speaker details
    * feat: Add speaker details
    
    * fix: Resolve PR changes

--- src/components/Speakers/Speakers.tsx
+++ src/components/Speakers/Speakers.tsx
@@ -1,27 +1,36 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 
-import { SpeakerType } from 'types'
-import Speaker from './Speaker'
+import { SpeakersType, SpeakerType } from 'types'
+import SpeakerCard from './SpeakerCard'
+import SpeakerDetails from './SpeakerDetails'
 
-type SpeakersType = {
-  speakers: SpeakerType[],
-  sectionTittle: string,
-}
+const Speakers: FC<SpeakersType> = ({ speakers, sectionTitle }) => {
+  const titleId = sectionTitle ? sectionTitle.toLocaleLowerCase() : ''
+  const [speaker, setSpeaker] = useState<SpeakerType | null>(null)
 
-const Speakers: FC<SpeakersType> = ({ speakers, sectionTittle }) => {
+  const handleSetSpeaker = (speaker: SpeakerType) => setSpeaker(speaker)
+
   return (
-    <div className="speakers" id={sectionTittle.toLowerCase()}>
-      <div className="speakers__inner">
-        <h1 className="speakers__header">{sectionTittle}</h1>
-        <div className="speakers__list">
-          {speakers.length ? (
-            speakers.map((speaker: SpeakerType) => <Speaker key={speaker.id} speaker={speaker} />)
-          ) : (
-            <div>Brak prelegentów</div>
-          )}
+    <>
+      <div className="speakers" id={titleId}>
+        <div className="speakers__inner">
+          <h1 className="speakers__header">{sectionTitle}</h1>
+          <div className="speakers__list">
+            {speakers ? (
+              speakers.map(
+                (speaker) =>
+                  speaker && (
+                    <SpeakerCard key={speaker.id} handleClick={() => handleSetSpeaker(speaker)} speaker={speaker} />
+                  ),
+              )
+            ) : (
+              <div>Brak prelegentów</div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+      <SpeakerDetails handleToggleModal={() => setSpeaker(null)} isOpen={!!speaker} speaker={speaker} />
+    </>
   )
 }
 export default Speakers
--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -22,3 +22,6 @@ export const links = [
     Icon: YoutubeIcon,
   },
 ]
+
+export const ImagePlaceholderUrl =
+  "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 280 342' width='280' height='342'%3E%3Crect width='280' height='342' fill='%23359960'%3E%3C/rect%3E%3Ctext x='50%25' y='50%25' dominant-baseline='middle' text-anchor='middle' font-family='monospace' font-size='26px' fill='%23ffffff'%3EBrak zdjęcia%3C/text%3E%3C/svg%3E"
