717182c02e579c754229bcdc5c93aed41a4d6e8b
jaworskig00 <2000grzegorz@gmail.com>
2022-07-05 12:00:45 +0200

    fix: PR requested changes

--- src/components/Schedule/ScheduleMobile/ScheduleMobile.tsx
+++ src/components/Schedule/ScheduleMobile/ScheduleMobile.tsx
@@ -11,40 +11,34 @@ const ScheduleMobile: FC<ScheduleType> = ({ scheduleTitle, events }) => {
       <h1 className="schedule-mobile__header">{scheduleTitle}</h1>
       <ScheduleCalendar />
       <div className="schedule-mobile__grid">
-        {ScheduleWeek.map(({ dayMobile, dateMobile }, i) => {
-          return (
-            <div key={dateMobile} className="schedule-mobile__grid-date" style={{ gridColumn: 1, gridRow: i + 1 }}>
-              {dateMobile}
-              <div className="schedule-mobile__grid-date-day">{dayMobile}</div>
-            </div>
-          )
-        })}
-        {ScheduleWeek.map(({ dateMobile }, i) => {
-          return (
-            <div key={i} className="schedule-mobile__grid-events" style={{ gridColumn: 2, gridRow: i + 1 }}>
-              {events &&
-                events.map((event) => {
-                  if (dateMobile === (event?.date && event?.date.substring(0, 2))) {
-                    return (
-                      <ScheduleEventMobile
-                        key={event?.id}
-                        bgColor={event?.backgroundColor}
-                        endHour={event?.endHour}
-                        startHour={event?.startHour}
-                        title={event?.title}
-                      />
-                    )
-                  }
-                  return null
-                })}
-            </div>
-          )
-        })}
-        {Array.from({ length: 10 }, (_, y) => {
-          return (
-            <div className="schedule-mobile__grid-placeholder" style={{ gridColumn: 2, gridRow: `${y + 1}` }}></div>
-          )
-        })}
+        {ScheduleWeek.map(({ dayMobile, dateMobile }, i) => (
+          <div key={dateMobile} className="schedule-mobile__grid-date" style={{ gridRow: i + 1 }}>
+            {dateMobile}
+            <div className="schedule-mobile__grid-date-day">{dayMobile}</div>
+          </div>
+        ))}
+        {ScheduleWeek.map(({ dateMobile }, i) => (
+          <div key={i} className="schedule-mobile__grid-events" style={{ gridRow: i + 1 }}>
+            {events?.length > 0 &&
+              events.map((event) => {
+                if (dateMobile === (event.date && event.date.substring(0, 2))) {
+                  return (
+                    <ScheduleEventMobile
+                      key={event.id}
+                      bgColor={event.backgroundColor}
+                      endHour={event.endHour}
+                      startHour={event.startHour}
+                      title={event.title}
+                    />
+                  )
+                }
+                return null
+              })}
+          </div>
+        ))}
+        {Array.from({ length: 10 }, (_, y) => (
+          <div className="schedule-mobile__grid-placeholder" style={{ gridColumn: 2, gridRow: `${y + 1}` }}></div>
+        ))}
       </div>
     </div>
   )
