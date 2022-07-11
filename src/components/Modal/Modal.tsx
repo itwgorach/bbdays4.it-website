24315aa2c54415edbc0e371e9d7adfffb0227d4e
jaworskig00 <2000grzegorz@gmail.com>
2022-07-11 19:42:04 +0200

    fix: modal position, blur and button

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -12,13 +12,7 @@ type ModalProps = {
 
 const Modal = ({ children, handleToggle, title, isOpen, className }: ModalProps) => {
   return (
-    <Dialog
-      allowPinchZoom
-      aria-label={title}
-      className={className}
-      isOpen={isOpen}
-      onClick={handleToggle}
-      onDismiss={handleToggle}>
+    <Dialog allowPinchZoom aria-label={title} className={className} isOpen={isOpen} onDismiss={handleToggle}>
       {children}
     </Dialog>
   )
