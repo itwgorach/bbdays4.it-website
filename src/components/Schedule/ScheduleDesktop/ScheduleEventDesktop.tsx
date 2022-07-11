38bc0daa09a40bf2c24dc457f86fbb278940d069
Grzegorz Jaworski <35576532+jaworskig00@users.noreply.github.com>
2022-07-11 17:54:35 +0200

    feat: add possibility for concurrent events
    * fix: concurrent events
    
    * fix: pr feedback

--- src/components/Schedule/ScheduleDesktop/ScheduleEventDesktop.tsx
+++ src/components/Schedule/ScheduleDesktop/ScheduleEventDesktop.tsx
@@ -1,17 +1,28 @@
-import { Hours, ScheduleWeek } from 'constants/constants'
-import React from 'react'
+import { Hours } from 'constants/constants'
+import React, { FC } from 'react'
+import { EventType } from 'types'
 
-const ScheduleEventDesktop = ({ bgColor, startHour, endHour, date, logo, title, displayTitleOnDesktop }) => {
-  const startY = Hours.findIndex((h) => h === startHour) + 2
-  const endY = endHour !== '23:00' ? Hours.findIndex((h) => h === endHour) + 2 : 15
-  const x = ScheduleWeek.findIndex((obj) => obj.dateDesktop === date) + 2
+type ScheduleEventType = {
+  event: EventType,
+  gridColumnNumber: number,
+  startParentGridColumn: number,
+}
+
+const ScheduleEventDesktop: FC<ScheduleEventType> = ({
+  event: { backgroundColor, endHour, startHour, title, logo, displayTitleOnDesktop },
+  gridColumnNumber,
+  startParentGridColumn,
+}) => {
+  const eventHours = Hours.slice(startParentGridColumn - 1, startParentGridColumn + gridColumnNumber - 1)
+  const startGridColumn = eventHours.findIndex((h) => h === startHour) + 2
+  const endGridColumn = endHour !== '23:00' ? eventHours.findIndex((h) => h === endHour) + 2 : 13
 
   return (
     <div
-      className="schedule-desktop__grid-event"
-      style={{ backgroundColor: bgColor, gridColumn: `${startY} / ${endY}`, gridRow: x }}>
-      {displayTitleOnDesktop && <div className="schedule-desktop__grid-event-title">{title}</div>}
-      {logo && <img alt="Logo" className="schedule-desktop__grid-event-logo" src={logo?.url} />}
+      className="schedule-desktop__grid-events-event"
+      style={{ backgroundColor: backgroundColor, gridColumn: `${startGridColumn} / ${endGridColumn}` }}>
+      {displayTitleOnDesktop && <div className="schedule-desktop__grid-events-event-title">{title}</div>}
+      {logo && <img alt="Logo" className="schedule-desktop__grid-events-event-logo" src={logo.url} />}
     </div>
   )
 }
