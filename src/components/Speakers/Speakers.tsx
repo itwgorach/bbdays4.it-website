1dd95154181fbc6a654868144a42a926dd701bed
Patryk <47920348+Matasin@users.noreply.github.com>
2022-07-14 17:34:12 +0200

    feat:  speakers redesign (#42)
    * feat: speakers design
    
    * feat: remove speaker modal
    
    * fix: resolve pr feedback | rebased
    
    * fix: spacer between speakers on xl
    
    * fix: spacer between speakers on xl

--- src/components/Speakers/Speakers.tsx
+++ src/components/Speakers/Speakers.tsx
@@ -1,36 +1,29 @@
-import React, { FC, useState } from 'react'
+import React, { FC } from 'react'
 
-import { SpeakersType, SpeakerType } from 'types'
+import { SpeakersType } from 'types'
 import SpeakerCard from './SpeakerCard'
-import SpeakerDetails from './SpeakerDetails'
 
-const Speakers: FC<SpeakersType> = ({ speakers, sectionTitle }) => {
+const Speakers: FC<SpeakersType> = ({ speakers, sectionTitle, sectionSubtitle }) => {
   const titleId = sectionTitle ? sectionTitle.toLocaleLowerCase() : ''
-  const [speaker, setSpeaker] = useState<SpeakerType | null>(null)
-
-  const handleSetSpeaker = (speaker: SpeakerType) => setSpeaker(speaker)
 
   return (
-    <>
-      <div className="speakers" id={titleId}>
-        <div className="speakers__inner">
-          <h1 className="speakers__header">{sectionTitle}</h1>
-          <div className="speakers__list">
-            {speakers ? (
-              speakers.map(
-                (speaker) =>
-                  speaker && (
-                    <SpeakerCard key={speaker.id} handleClick={() => handleSetSpeaker(speaker)} speaker={speaker} />
-                  ),
-              )
-            ) : (
-              <div>Brak prelegentów</div>
-            )}
-          </div>
+    <div className="speakers" id={titleId}>
+      <div className="speakers__inner">
+        <div className="speakers__header">
+          <h1 className="speakers__title">{sectionTitle}</h1>
+          <h4 className="speakers__subtitle">{sectionSubtitle}</h4>
         </div>
+        <div className="speakers__spacer" />
+        <div className="speakers__list">
+          {speakers ? (
+            speakers.map((speaker) => speaker && <SpeakerCard key={speaker.id} speaker={speaker} />)
+          ) : (
+            <div>Brak prelegentów</div>
+          )}
+        </div>
+        <div className="speakers__spacer -down" />
       </div>
-      <SpeakerDetails handleToggleModal={() => setSpeaker(null)} isOpen={!!speaker} speaker={speaker} />
-    </>
+    </div>
   )
 }
 export default Speakers
