5d8db4a43fe526ca053b67862deb2a1c0721163e
Tomasz Kielar <32620457+k1eu@users.noreply.github.com>
2022-08-01 17:36:14 +0200

    Release v1.3.0
    * feat: registration buttons
    
    * feat: registration buttons
    
    * fix: checking for first hero
    
    * fix: extend types from strapi
    
    * fix: buttons mapping
    
    * docs: bump version to 1.3.0
    
    Co-authored-by: Grzegorz Jaworski <35576532+jaworskig00@users.noreply.github.com>

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,14 +3,32 @@ import React, { FC } from 'react'
 
 import { BaseHeroType } from 'types'
 
-const Hero: FC<BaseHeroType> = ({
+type HeroProps = BaseHeroType & {
+  isFirstHero: boolean,
+}
+
+const RegistrationLinks = [
+  {
+    href: 'https://app.evenea.pl/event/bbdays4it-2022-konferencja-ATH',
+    id: 0,
+    text: 'Zapisy na Konferencję',
+  },
+  {
+    href: 'https://app.evenea.pl/event/bbq4it-2022',
+    id: 1,
+    text: 'Zapisy na BBQ4IT',
+  },
+]
+
+const Hero: FC<HeroProps> = ({
   title,
   subtitle,
   subtitlePos,
   backgroundImage,
+  backgroundColor,
   buttonText,
   buttonUrl,
-  backgroundColor,
+  isFirstHero,
 }) => {
   const hasButton = buttonText && buttonUrl
   const hasSubtitle = subtitle
@@ -29,10 +47,19 @@ const Hero: FC<BaseHeroType> = ({
       {hasSubtitle && subtitlePos === 'top' && <h3 className="hero__subtitle">{subtitle}</h3>}
       <h1 className={titleClasses}>{title}</h1>
       {hasSubtitle && subtitlePos === 'bottom' && <h3 className="hero__subtitle -bottom">{subtitle}</h3>}
-      {hasButton && (
+      {!isFirstHero && hasButton && (
         <a className="hero__button" href={buttonUrl} rel="noopener noreferrer" target="_blank">
           {buttonText}
         </a>
+      )}
+      {isFirstHero && (
+        <div className="hero__button-wrapper">
+          {RegistrationLinks.map(({ href, id, text }) => (
+            <a key={id} className="hero__button" href={href} rel="noopener noreferrer" target="_blank">
+              {text}
+            </a>
+          ))}
+        </div>
       )}
     </div>
   )
