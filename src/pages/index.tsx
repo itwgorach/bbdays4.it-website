a4bf137a19b7b982f8de3bb719e9b0e0c7343f46
Grzegorz Jaworski <35576532+jaworskig00@users.noreply.github.com>
2022-08-08 17:05:04 +0200

    feat: signup section component
    * feat: signup section component
    
    * feat: signup section mobile and improvements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,12 @@ import { graphql } from 'gatsby'
 
 import HomePageType from 'types/HomePageType'
 import Hero from 'components/Hero'
-import { BaseGalleryType, BaseHeroType, PartnersType, SpeakersType, ScheduleType } from 'types'
+import { BaseGalleryType, BaseHeroType, PartnersType, SpeakersType, ScheduleType, SignupType } from 'types'
 import Partners from 'components/Partners'
 import Gallery from 'components/Gallery'
 import Speakers from 'components/Speakers'
 import Schedule from 'components/Schedule'
+import Signup from 'components/Signup'
 
 const HomePage: FC<HomePageType> = ({
   data: {
@@ -40,6 +41,10 @@ const HomePage: FC<HomePageType> = ({
           case 'base.schedule': {
             const schedule = component as ScheduleType
             return <Schedule key={schedule.id} {...schedule} />
+          }
+          case 'base.signup-grid': {
+            const signup = component as SignupType
+            return <Signup key={signup.id} {...signup} />
           }
           default:
             return null
@@ -134,6 +139,23 @@ export const query = graphql`
                       logo {
                           url
                       }
+                  }
+              }
+              ... on STRAPI__COMPONENT_BASE_SIGNUP_GRID {
+                  id
+                  signupImage {
+                    url
+                  }
+                  bannerImage {
+                    url
+                  }
+                  strapi_component
+                  isSectionVisible
+                  signup_sections {
+                      buttonText
+                      buttonUrl
+                      title
+                      subtitle
                   }
               }
           }
