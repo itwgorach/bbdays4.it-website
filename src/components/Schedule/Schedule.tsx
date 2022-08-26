db642bf6602f26819f42c1db7e4b442fb0770cbb
Grzegorz Jaworski <35576532+jaworskig00@users.noreply.github.com>
2022-08-26 14:38:12 +0200

    feat: header highlight links
    * feat: install waypoint library
    
    * feat: active link functionality
    
    * feat: export context to separate file
    
    * fix: schedule highlight
    
    * fix: partners highlight

--- src/components/Schedule/Schedule.tsx
+++ src/components/Schedule/Schedule.tsx
@@ -1,4 +1,6 @@
 import React, { FC } from 'react'
+import { useActiveLink } from 'contexts/ActiveLinkContext'
+import { Waypoint } from 'react-waypoint'
 import { ScheduleType } from 'types'
 
 import ScheduleDesktop from './ScheduleDesktop/ScheduleDesktop'
@@ -6,11 +8,24 @@ import ScheduleMobile from './ScheduleMobile/ScheduleMobile'
 
 const Schedule: FC<ScheduleType> = ({ events, scheduleTitle }) => {
   const titleId = scheduleTitle ? scheduleTitle.toLocaleLowerCase() : ''
+  const { setActiveLink } = useActiveLink()
 
   return (
     <div className="schedule" id={titleId}>
-      <ScheduleMobile events={events} scheduleTitle={scheduleTitle} />
-      <ScheduleDesktop events={events} scheduleTitle={scheduleTitle} />
+      <Waypoint
+        bottomOffset="60%"
+        topOffset="-200%"
+        onEnter={() => {
+          setActiveLink(titleId)
+        }}
+        onLeave={() => {
+          setActiveLink('')
+        }}>
+        <div>
+          <ScheduleMobile events={events} scheduleTitle={scheduleTitle} />
+          <ScheduleDesktop events={events} scheduleTitle={scheduleTitle} />
+        </div>
+      </Waypoint>
     </div>
   )
 }
--- src/components/Speakers/Speakers.tsx
+++ src/components/Speakers/Speakers.tsx
@@ -1,4 +1,6 @@
 import React, { FC } from 'react'
+import { useActiveLink } from 'contexts/ActiveLinkContext'
+import { Waypoint } from 'react-waypoint'
 
 import { SpeakersType } from 'types'
 import SpeakerCard from './SpeakerCard'
@@ -6,25 +8,36 @@ import SpeakerCommingSoon from './SpeakerComingSoon'
 
 const Speakers: FC<SpeakersType> = ({ speakers, sectionTitle, sectionSubtitle }) => {
   const titleId = sectionTitle ? sectionTitle.toLocaleLowerCase() : ''
+  const { setActiveLink } = useActiveLink()
 
   return (
-    <div className="speakers" id={titleId}>
-      <div className="speakers__inner">
-        <div className="speakers__header">
-          <h1 className="speakers__title">{sectionTitle}</h1>
-          <h4 className="speakers__subtitle">{sectionSubtitle}</h4>
+    <Waypoint
+      bottomOffset="60%"
+      topOffset="39%"
+      onEnter={() => {
+        setActiveLink(titleId)
+      }}
+      onLeave={() => {
+        setActiveLink('')
+      }}>
+      <div className="speakers" id={titleId}>
+        <div className="speakers__inner">
+          <div className="speakers__header">
+            <h1 className="speakers__title">{sectionTitle}</h1>
+            <h4 className="speakers__subtitle">{sectionSubtitle}</h4>
+          </div>
+          <div className="speakers__list">
+            {speakers ? (
+              speakers.map((speaker) => speaker && <SpeakerCard key={speaker.id} speaker={speaker} />)
+            ) : (
+              <div>Brak prelegentów</div>
+            )}
+            <SpeakerCommingSoon />
+          </div>
+          <div className="speakers__spacer -down" />
         </div>
-        <div className="speakers__list">
-          {speakers ? (
-            speakers.map((speaker) => speaker && <SpeakerCard key={speaker.id} speaker={speaker} />)
-          ) : (
-            <div>Brak prelegentów</div>
-          )}
-          <SpeakerCommingSoon />
-        </div>
-        <div className="speakers__spacer -down" />
       </div>
-    </div>
+    </Waypoint>
   )
 }
 export default Speakers
