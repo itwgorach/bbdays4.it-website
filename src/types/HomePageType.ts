7d9f4522c79660d689a7b8319fbd6dd97e27a77a
Tomasz Kielar <32620457+k1eu@users.noreply.github.com>
2022-08-26 11:29:16 +0200

    Release v1.6.0
    * fix: agenda styles changes
    
    * feat: details modal functionality
    
    * feat: details modal mobile styling
    
    * feat: details modal desktop styling
    
    * feat: speakers and lectures colors from strapi
    
    * feat: details modal prev/next buttons functionality
    
    * feat: details modal url handling
    
    * feat: code pre-refactoring
    
    * fix: pr requested changes
    
    * fix: css files sort alphabetically
    
    * fix: speaker image and details modal animations
    
    * fix: delete console log
    
    Co-authored-by: Grzegorz Jaworski <35576532+jaworskig00@users.noreply.github.com>

--- src/types/HomePageType.ts
+++ src/types/HomePageType.ts
@@ -3,7 +3,8 @@ import STRAPI_HOMEPAGE = GatsbyTypes.STRAPI_HOMEPAGE
 type HomePageType = {
   data:{
     strapiHomepage: STRAPI_HOMEPAGE
-  }
+  },
+  location: {},
 }
 
 export default HomePageType
