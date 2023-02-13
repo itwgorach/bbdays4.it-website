496d631f2cd7d89c2166b72c92118bdf47941609
matgorn <m.gorny@selleo.com>
2023-02-13 14:56:18 +0100

    chore: add index to grapqhl query and func to sort data

--- src/components/Speakers/Speakers.tsx
+++ src/components/Speakers/Speakers.tsx
@@ -3,12 +3,15 @@ import { useActiveLink } from 'contexts/ActiveLinkContext'
 import { Waypoint } from 'react-waypoint'
 
 import { SpeakersType } from 'types'
+import { getSortedByIndex } from 'utils/getSortedSpeakers'
 import SpeakerCard from './SpeakerCard'
 import SpeakerCommingSoon from './SpeakerComingSoon'
 
 const Speakers: FC<SpeakersType> = ({ speakers, sectionTitle, sectionSubtitle }) => {
   const titleId = sectionTitle ? sectionTitle.toLocaleLowerCase() : ''
   const { setActiveLink } = useActiveLink()
+
+  const sortedSpeakers = Array.isArray(speakers) && getSortedByIndex(speakers)
 
   return (
     <Waypoint
@@ -27,8 +30,8 @@ const Speakers: FC<SpeakersType> = ({ speakers, sectionTitle, sectionSubtitle })
             <h4 className="speakers__subtitle">{sectionSubtitle}</h4>
           </div>
           <div className="speakers__list">
-            {speakers ? (
-              speakers.map((speaker) => speaker && <SpeakerCard key={speaker.id} speaker={speaker} />)
+            {sortedSpeakers ? (
+              sortedSpeakers.map((speaker) => speaker && <SpeakerCard key={speaker.id} speaker={speaker} />)
             ) : (
               <div>Brak prelegentów</div>
             )}
