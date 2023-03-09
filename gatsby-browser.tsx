bbd323aba48d01f1d41483b554b1d863cf0f3820
matgorn <m.gorny@selleo.com>
2023-03-09 11:12:49 +0100

    fix: gatsby cache prevents loading updated content

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -6,3 +6,6 @@ require('styles/app.scss')
 exports.wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
 }
+
+// trigger an immediate page refresh when an update is found
+exports.onServiceWorkerUpdateReady = () => window.location.reload()
