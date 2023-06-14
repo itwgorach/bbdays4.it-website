6c4a1181aa4adfd8240bae71a9be53ce7acf8984
Grzegorz Jaworski <35576532+jaworskig00@users.noreply.github.com>
2023-06-14 16:25:54 +0200

    fix: agenda sorting and modals (#111)
    * fix: agenda sorting
    
    * feat: disable lecture onClick
    
    * feat: hide "become speaker" from header

--- src/components/Agenda/AgendaSection.tsx
+++ src/components/Agenda/AgendaSection.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import cx from 'classnames'
 import { getSpeaker } from 'utils/agendaDataProcessing'
 
-const AgendaSection = ({ section, handleModalToggle, speakers }) => {
+const AgendaSection = ({ lectures, handleModalToggle, speakers }) => {
   const getLectureContentClasses = (backgroundColor: string | null, logoUrl: string | null) => {
     const lectureContentClasses = cx('agenda__lecture-content', {
       [`-${backgroundColor}`]: backgroundColor,
@@ -29,33 +29,25 @@ const AgendaSection = ({ section, handleModalToggle, speakers }) => {
 
   return (
     <>
-      {Object.keys(section).map((key, index) => {
+      {lectures.map(({ backgroundColor, title, subtitle, room, logo, startHour }, index) => {
+        // const modalProps = subtitle && { ...getSpeaker(subtitle, speakers), hour: startHour, room: room }
+
         return (
           <div key={index} className="agenda__lecture-wrapper">
-            <div className="agenda__lecture-hour">{key}</div>
+            <div className="agenda__lecture-hour">{startHour}</div>
             <div className="agenda__lecture-inner">
-              {section[key].map(({ backgroundColor, title, subtitle, room, logo }, idx) => {
-                const modalProps = subtitle && { ...getSpeaker(subtitle, speakers), hour: key, room: room }
-
-                return (
-                  <div
-                    key={idx}
-                    className={getLectureClasses(room)}
-                    id={subtitle && `agenda/${getLectureId(subtitle)}`}>
-                    <div
-                      className={getLectureContentClasses(backgroundColor, logo && logo.url)}
-                      onClick={subtitle ? (event) => handleModalToggle(event, { ...modalProps }) : undefined}>
-                      {subtitle && <span className="agenda__lecture-subtitle">{subtitle}</span>}
-                      <span className="agenda__lecture-title">{title}</span>
-                      {logo && (
-                        <span className="agenda__lecture-logo">
-                          <img alt="Logo" src={logo.url} />
-                        </span>
-                      )}
-                    </div>
-                  </div>
-                )
-              })}
+              <div key={index} className={getLectureClasses(room)} id={subtitle && `agenda/${getLectureId(subtitle)}`}>
+                <div className={getLectureContentClasses(backgroundColor, logo && logo.url)}>
+                  {/* {onClick={subtitle ? (event) => handleModalToggle(event, { ...modalProps }) : undefined}>} */}
+                  {subtitle && <span className="agenda__lecture-subtitle">{subtitle}</span>}
+                  <span className="agenda__lecture-title">{title}</span>
+                  {logo && (
+                    <span className="agenda__lecture-logo">
+                      <img alt="Logo" src={logo.url} />
+                    </span>
+                  )}
+                </div>
+              </div>
             </div>
           </div>
         )
