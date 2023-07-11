31027cd909d807cd37d28b5e8b5dd036ad30ef80
Grzegorz Jaworski <g.jaworski@selleo.com>
2023-07-11 12:56:42 +0200

    feat: unlock agenda modals

--- src/components/Agenda/AgendaSection.tsx
+++ src/components/Agenda/AgendaSection.tsx
@@ -30,15 +30,16 @@ const AgendaSection = ({ lectures, handleModalToggle, speakers }) => {
   return (
     <>
       {lectures.map(({ backgroundColor, title, subtitle, room, logo, startHour }, index) => {
-        // const modalProps = subtitle && { ...getSpeaker(subtitle, speakers), hour: startHour, room: room }
+        const modalProps = subtitle && { ...getSpeaker(subtitle, speakers), hour: startHour, room: room }
 
         return (
           <div key={index} className="agenda__lecture-wrapper">
             <div className="agenda__lecture-hour">{startHour}</div>
             <div className="agenda__lecture-inner">
               <div key={index} className={getLectureClasses(room)} id={subtitle && `agenda/${getLectureId(subtitle)}`}>
-                <div className={getLectureContentClasses(backgroundColor, logo && logo.url)}>
-                  {/* {onClick={subtitle ? (event) => handleModalToggle(event, { ...modalProps }) : undefined}>} */}
+                <div
+                  className={getLectureContentClasses(backgroundColor, logo && logo.url)}
+                  onClick={subtitle ? (event) => handleModalToggle(event, { ...modalProps }) : undefined}>
                   {subtitle && <span className="agenda__lecture-subtitle">{subtitle}</span>}
                   <span className="agenda__lecture-title">{title}</span>
                   {logo && (
