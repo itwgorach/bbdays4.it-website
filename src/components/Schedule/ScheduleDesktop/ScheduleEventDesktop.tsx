e640fa89b0c47b0e4ac402a04634f66e022c51da
Grzegorz Jaworski <g.jaworski@selleo.com>
2023-07-19 15:16:15 +0200

    fix: displaying event on schedule

--- src/components/Schedule/ScheduleDesktop/ScheduleEventDesktop.tsx
+++ src/components/Schedule/ScheduleDesktop/ScheduleEventDesktop.tsx
@@ -16,10 +16,15 @@ const ScheduleEventDesktop: FC<ScheduleEventType> = ({
   isConcurrent,
   startParentGridColumn,
 }) => {
-  const eventHours = Hours.slice(startParentGridColumn - 1, startParentGridColumn + gridColumnNumber - 1)
-  const startGridColumn = eventHours.findIndex((h) => h === startHour) + 2
-  const endGridColumn = endHour !== '22:00' ? eventHours.findIndex((h) => h === endHour) + 2 : 13
-  const isOneHourLong = endGridColumn - startGridColumn === 1
+  const eventHours = Hours.slice(startParentGridColumn / 2 - 1, (startParentGridColumn + gridColumnNumber) / 2)
+
+  const startHourIndex = eventHours.findIndex((hour) => hour.split(':')[0] === startHour?.split(':')[0])
+  const startGridColumn = (startHourIndex === -1 ? eventHours.length * 2 : startHourIndex * 2) + 1
+
+  const endHourIndex = eventHours.findIndex((hour) => hour.split(':')[0] === endHour?.split(':')[0])
+  const endGridColumn = (endHourIndex === -1 ? eventHours.length * 2 : endHourIndex * 2) + 1
+
+  const isOneHourLong = (endGridColumn - startGridColumn) / 2 === 1
   const shouldDisplayLogo = !(isOneHourLong && isConcurrent) && logo
 
   const eventClasses = cx('schedule-desktop__grid-events-event', {
@@ -28,12 +33,15 @@ const ScheduleEventDesktop: FC<ScheduleEventType> = ({
 
   return (
     <>
-      {link ? (
+      {link && link !== '/' ? (
         <a
           className={eventClasses}
           href={link}
           rel="noopener noreferrer"
-          style={{ backgroundColor: backgroundColor, gridColumn: `${startGridColumn} / ${endGridColumn}` }}
+          style={{
+            backgroundColor: backgroundColor,
+            gridColumn: `${startGridColumn} / ${endGridColumn}`,
+          }}
           target="_blank">
           {displayTitleOnDesktop && <div className="schedule-desktop__grid-events-event-title">{title}</div>}
           {shouldDisplayLogo && <img alt="Logo" className="schedule-desktop__grid-events-event-logo" src={logo.url} />}
@@ -41,7 +49,10 @@ const ScheduleEventDesktop: FC<ScheduleEventType> = ({
       ) : (
         <div
           className={eventClasses}
-          style={{ backgroundColor: backgroundColor, gridColumn: `${startGridColumn} / ${endGridColumn}` }}>
+          style={{
+            backgroundColor: backgroundColor,
+            gridColumn: `${startGridColumn} / ${endGridColumn}`,
+          }}>
           {displayTitleOnDesktop && <div className="schedule-desktop__grid-events-event-title">{title}</div>}
           {shouldDisplayLogo && <img alt="Logo" className="schedule-desktop__grid-events-event-logo" src={logo.url} />}
         </div>
