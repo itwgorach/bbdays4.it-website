a5fdb0109d83eb9d5e32bd0b303868c0eb541946
MateuszGacek <mateusz_gacek@interia.pl>
2024-05-17 11:15:12 +0200

    feat: create page with videos

---
+++ src/pages/videos/videos.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const videos = () => {
+  return (
+    <>
+      <h1>Youtube</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, commodi?</p>
+    </>
+  )
+}
+
+export default videos
