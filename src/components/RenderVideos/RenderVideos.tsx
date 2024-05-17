d9dff436a67b265968588e15b88f2fb19e49986e
MateuszGacek <mateusz_gacek@interia.pl>
2024-05-17 14:43:56 +0200

    feat: create video component

--- src/components/RenderVideos/RenderVideos.tsx
+++ src/components/RenderVideos/RenderVideos.tsx
@@ -3,9 +3,8 @@ import * as React from 'react'
 const RenderVideos = ({ videosData }) => {
   return videosData.map((video) => (
     <iframe
-      width="80%"
+      className="video__controler-video"
       src={video}
-      title="YouTube video player"
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       referrerpolicy="strict-origin-when-cross-origin"
--- src/pages/videos/videos.tsx
+++ src/pages/videos/videos.tsx
@@ -5,13 +5,15 @@ const videosData = [
   'https://www.youtube.com/embed/Iwd9oP3B96U?si=LAMamqU-RZepmxJG',
   'https://www.youtube.com/embed/Iwd9oP3B96U?si=LAMamqU-RZepmxJG',
   'https://www.youtube.com/embed/Iwd9oP3B96U?si=LAMamqU-RZepmxJG',
+  'https://www.youtube.com/embed/Iwd9oP3B96U?si=LAMamqU-RZepmxJG',
+  'https://www.youtube.com/embed/Iwd9oP3B96U?si=LAMamqU-RZepmxJG',
 ]
 
 const videos = () => {
   return (
     <>
       <h1>Youtube playlist</h1>
-      <div className="video-controler">
+      <div className="videos__controler">
         <RenderVideos videosData={videosData} />
       </div>
     </>
