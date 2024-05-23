0ffbccdafee2c3885eaa0ba2b2523a6316ed0e11
MateuszGacek <mateusz_gacek@interia.pl>
2024-05-23 10:57:03 +0200

    feat: display events on live

--- src/components/Schedule/ScheduleLiveInformation/ScheduleLiveInformation.tsx
+++ src/components/Schedule/ScheduleLiveInformation/ScheduleLiveInformation.tsx
@@ -2,25 +2,36 @@ import React from 'react'
 import { checkEventIsActive } from 'utils/checkEventIsActive'
 
 type Event = {
-  id: string,
-  title: string,
-  startHour: string,
-  endHour: string,
-  date: string,
-  backgroundColor: string,
-  displayTitleOnDesktop: boolean,
-  link: string,
-  logo: null,
+  id: string
+  title: string
+  startHour: string
+  endHour: string
+  date: string
+  backgroundColor: string
+  displayTitleOnDesktop: boolean
+  link: string
+  logo: null
 }
 
 const ScheduleLiveInformation = ({ events }: { events: [Event] }) => {
+  if (!events) {
+    return null
+  }
+
   return (
-    <>
+    <div className="schedule__live">
       {events.map((event) => {
         const { date, startHour, endHour } = event
-        return checkEventIsActive(date, startHour, endHour) && <p>Event details</p>
+        const isActive = checkEventIsActive(date, startHour, endHour)
+        return (
+          isActive && (
+            <div className="schedule__live-item" style={{ background: `${event.backgroundColor}` }}>
+              <p className="schedule__live-title">{event.title}</p>
+            </div>
+          )
+        )
       })}
-    </>
+    </div>
   )
 }
 
