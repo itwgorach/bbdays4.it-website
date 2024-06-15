ef4f7d7426578088be4911035318fa5cdba8373d
MateuszGacek <mateusz_gacek@interia.pl>
2024-06-15 17:12:37 +0200

    feat: display and styled btn to change language

--- src/components/Layout/Language.tsx
+++ src/components/Layout/Language.tsx
@@ -2,12 +2,15 @@ import { useLanguageContext } from 'contexts/LanguageContext'
 import React from 'react'
 
 const Language = () => {
-  const { toggleLanguage } = useLanguageContext()
+  const { toggleLanguage, language } = useLanguageContext()
+  const displayLangBtn = language === 'pl' ? 'en' : 'pl'
 
   return (
-    <div className="language">
-      <button onClick={toggleLanguage}>Language</button>
-    </div>
+    <>
+      <button className="language" onClick={toggleLanguage}>
+        {displayLangBtn}
+      </button>
+    </>
   )
 }
 export default Language
