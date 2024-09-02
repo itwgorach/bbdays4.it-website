2b12ec9a61fa0ab648bd06ea883e6d4286630a7e
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2024-09-02 10:42:44 +0200

    feat: improve speaker card modal (#147)
    * feat: improve footer of the card
    
    * feat: add accent box
    
    * feat: adjust typography and spacings
    
    * feat: create wrapper
    
    * refactor: remove second accent

--- src/components/Speakers/SpeakerCard.tsx
+++ src/components/Speakers/SpeakerCard.tsx
@@ -45,25 +45,24 @@ const SpeakerCard: FC<SpeakerCardProps> = ({ speaker }) => {
         <button className="speaker-card__modal-button-close" onClick={showModalFn}>
           <CloseButtonIcon />
         </button>
-        <div className={` ${backgroundColor}`}>
-          <div className="speaker-card__modal-spacing">
-            <div>
-              <div className="speaker-card__modal-name">{`${firstName} ${lastName}`}</div>
-              <div className="speaker-card__modal-status">{position}</div>
-            </div>
-            <div className="speaker-card__modal-icons">
-              {linkedinUrl && <SpeakerCardSocial href={linkedinUrl} type={'linkedin'} />}
-              {twitterUrl && <SpeakerCardSocial href={twitterUrl} type={'twitter'} />}
-              {linktrUrl && <SpeakerCardSocial href={linktrUrl} type={'linktr'} />}
-            </div>
-          </div>
-          <div className="speaker-card__modal-spacing">
+        <div className="speaker-card__modal-wrapper">
+          <div className="speaker-card__modal-author-details">
+            <div className={(backgroundColor || '') + ' speaker-card__modal-header-accent'} />
+            <div className="speaker-card__modal-status">{position}</div>
+            <div className="speaker-card__modal-name">{`${firstName} ${lastName}`}</div>
             <p className="speaker-card__modal-biography">{bio}</p>
-            <Image alt={fullName} url={imageUrl} />
           </div>
+          <div className="speaker-card__modal-icons">
+            {linkedinUrl && <SpeakerCardSocial href={linkedinUrl} type={'linkedin'} />}
+            {twitterUrl && <SpeakerCardSocial href={twitterUrl} type={'twitter'} />}
+            {linktrUrl && <SpeakerCardSocial href={linktrUrl} type={'linktr'} />}
+          </div>
+          <Image alt={fullName} url={imageUrl} />
         </div>
-        <h3 className="speaker-card__modal-title">{title}</h3>
-        <p className="speaker-card__modal-description">{description}</p>
+        <div className={(backgroundColor || '') + ' speaker-card__modal-lecture-details'}>
+          <h3 className="speaker-card__modal-title">{title}</h3>
+          <p className="speaker-card__modal-description">{description}</p>
+        </div>
       </Modal>
       <div className="speaker-card__content">
         <h3 className="speaker-card__title">
